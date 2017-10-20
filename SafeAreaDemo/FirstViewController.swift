e897ebfb83374f7f9bedb0e606edbf63bae26bfc
Matthew Thompson <matt.thompson412@gmail.com>
2017-10-19 22:17:34 -0500

    first commit

---
+++ SafeAreaDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SafeAreaDemo
+//
+//  Created by Matthew Thompson on 10/19/17.
+//  Copyright © 2017 McTap. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
